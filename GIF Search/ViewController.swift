64bf3bc1f4d090ab39f9b37101fa6d1b7b6b8652
Max Meyers <maxmeyers@me.com>
2020-04-26 20:05:43 -0700

    Initial commit

---
+++ GIF Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GIF Search
+//
+//  Created by Max on 4/26/20.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
